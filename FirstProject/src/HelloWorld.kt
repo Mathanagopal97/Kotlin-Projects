dd17ca9bf2e7b602a15bf24720ad04905d3bb75f
Mathanagopal Sankarasubramanian <mathug123@gmail.com>
2019-08-04 17:40:29 +0530

    Datatypes

--- FirstProject/src/HelloWorld.kt
+++ FirstProject/src/HelloWorld.kt
@@ -6,4 +6,15 @@ fun main()
     val unchangable = "This cannot be changed"
 
     print("$changable and $unchangable")
+
+
+    var b: Byte = 127
+    var s: Short = 1000
+    var i:Int = 12
+    var d:Double = 34.6
+    var bool:Boolean = true
+    var f:Float = 56.7f
+    var l:Long = 1234213432423421424
+
+    //toDouble() toLong() toInt() toShort()
 }
