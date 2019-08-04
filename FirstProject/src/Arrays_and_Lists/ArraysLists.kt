524e6c31f7823fe484e5df235ccb8b2fab294c67
Mathanagopal Sankarasubramanian <mathug123@gmail.com>
2019-08-04 22:59:25 +0530

    Arrays and Lists

---
+++ FirstProject/src/Arrays_and_Lists/ArraysLists.kt
@@ -0,0 +1,14 @@
+package Arrays_and_Lists
+
+fun main() {
+    //Arrays
+    val arrOfStates = arrayOf("Texas", "Iowa", "California", "Cincinnati")
+    val mixed = arrayOf("Hello", 1, 6.7, true, arrOfStates)
+
+    //intArrayOf(), doubleArrayOf(), booleanArrayOf(), floatArrayOf(), charArrayOf()
+
+    val listOfFruits = listOf("Apple", "Orange", "Mango", "Pineapple")
+
+    val arrayListofNames = arrayListOf("Patric", "Mike", "Eleven")
+    arrayListofNames.add("Samantha")
+}
