c2018208e87f201417f72c2fe9df5f779716ea27
Mathanagopal Sankarasubramanian <mathug123@gmail.com>
2019-08-04 20:00:22 +0530

    Conditionals Exercise

--- FirstProject/src/Conditionals/Conditionals.kt
+++ FirstProject/src/Conditionals/Conditionals.kt
@@ -1,5 +1,7 @@
 package Conditionals
 
+import java.util.*
+
 fun main(args: Array<String>) {
     var age:Int = 21
     if(age<18){
@@ -15,11 +17,25 @@ fun main(args: Array<String>) {
         1-> "Lazy mode"
         2->{
             "Mode 2" + " " +
-            "Not a lazy mode"
+                    "Not a lazy mode"
         }
         3->"Mode 3"
         else->"Don't know that mode"
     }
 
+    println(result)
+
+
+    //Conditionals Exercise
+    val random:Int = Random().nextInt(50)+1;
+
+     result = when(random){
+        in 1..10 -> "$random is Between 1 and 10"
+        in 11..20 -> "$random is Between 11 and 20"
+        in 20..30 -> "$random is Between 20 and 30"
+        in 30..40 -> "$random is Between 30 and 40"
+        in 40..50 -> "$random is Between 40 and 50"
+        else -> "$random is ND"
+    }
     println(result)
 }
