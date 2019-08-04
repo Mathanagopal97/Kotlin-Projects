b7b76e32179949fe92ea722e63efc6440049ef90
Mathanagopal Sankarasubramanian <mathug123@gmail.com>
2019-08-04 19:44:00 +0530

    When Conditionals

--- FirstProject/src/Conditionals/Conditionals.kt
+++ FirstProject/src/Conditionals/Conditionals.kt
@@ -8,4 +8,18 @@ fun main(args: Array<String>) {
     else{
         println("You can register")
     }
+
+    //When Conditionals
+    var mode:Int = 2
+    var result = when(mode){
+        1-> "Lazy mode"
+        2->{
+            "Mode 2" + " " +
+            "Not a lazy mode"
+        }
+        3->"Mode 3"
+        else->"Don't know that mode"
+    }
+
+    println(result)
 }
