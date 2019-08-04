90a55dad145e0556f7f140d8fafddf461a42f62d
Mathanagopal Sankarasubramanian <mathug123@gmail.com>
2019-08-04 17:15:59 +0530

    Val and Var

--- FirstProject/src/HelloWorld.kt
+++ FirstProject/src/HelloWorld.kt
@@ -1,4 +1,9 @@
 fun main()
 {
-    print("Hello World")
+    print("Hello World\n")
+
+    var changable = "This can be changed"
+    val unchangable = "This cannot be changed"
+
+    print("$changable and $unchangable")
 }
