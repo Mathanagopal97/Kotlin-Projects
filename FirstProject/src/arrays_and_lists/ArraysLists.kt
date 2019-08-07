9fb4c9ef8d85e8edcd63d687346e666a6b4dc171
Mathanagopal Sankarasubramanian <mathug123@gmail.com>
2019-08-07 11:28:25 +0530

    Arrays and Lists

--- FirstProject/src/arrays_and_lists/ArraysLists.kt
+++ FirstProject/src/arrays_and_lists/ArraysLists.kt
@@ -1,14 +1,26 @@
-package Arrays_and_Lists
+package arrays_and_lists
 
 fun main() {
     //Arrays
     val arrOfStates = arrayOf("Texas", "Iowa", "California", "Cincinnati")
     val mixed = arrayOf("Hello", 1, 6.7, true, arrOfStates)
-
+    println(mixed[0])
+    mixed[0] = 123.6
+    println(mixed[0])
     //intArrayOf(), doubleArrayOf(), booleanArrayOf(), floatArrayOf(), charArrayOf()
 
     val listOfFruits = listOf("Apple", "Orange", "Mango", "Pineapple")
-
+    var str = "Mathanagopal"
+    println(str[0].toLowerCase())
     val arrayListofNames = arrayListOf("Patric", "Mike", "Eleven")
     arrayListofNames.add("Samantha")
+    val array2 = arrayOf("Miami", "Newark", "New York")
+    val combinedArray = arrOfStates + array2
+    println(combinedArray.size)
+    var isEmpty: Boolean = str.isEmpty();
+    println(isEmpty)
+    str = ""
+    isEmpty = str.isEmpty()
+    println(isEmpty)
+
 }
